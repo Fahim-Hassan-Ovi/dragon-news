e26b5a49a1f5fee2106db6993433c88aee9ca3ff
Fahim-Hassan-Ovi <fahim.hassan.ovi@gmail.com>
2025-02-06 16:09:09 +0600

    Added the navbar

--- src/layout/HomeLayout.jsx
+++ src/layout/HomeLayout.jsx
@@ -1,16 +1,19 @@
 import Header from "../components/Header";
 import LatestNews from "../components/LatestNews";
+import Navbar from "../components/Navbar";
 
 const HomeLayout = () => {
     return (
-        <div>
+        <div className="font-poppins">
             <header>
                 <Header />
                 <section className="w-11/12 mx-auto">
                     <LatestNews />
                 </section>
             </header>
-            <nav></nav>
+            <nav className="w-11/12 mx-auto py-2">
+                <Navbar />
+            </nav>
             <main></main>
         </div>
     );
