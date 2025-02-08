3babe1633817b26bbac895514cd120d3a0c9cdc8
Fahim-Hassan-Ovi <fahim.hassan.ovi@gmail.com>
2025-02-08 21:44:07 +0600

    update user profile on registration and environment variable

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAD6NqfV39HlFYTA_QLpMI9OaUvCX8OyUM",
-  authDomain: "dragon-news-410c5.firebaseapp.com",
-  projectId: "dragon-news-410c5",
-  storageBucket: "dragon-news-410c5.firebasestorage.app",
-  messagingSenderId: "160567957041",
-  appId: "1:160567957041:web:9e7873ecf66e85bfd12710"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
